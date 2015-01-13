2e209ec97b06d4c854decb8b3deeeeadb6d8a792
thesamtang <linglinghost@gmail.com>
2015-01-13 01:04:03 -0500

    changed logo to menu button

--- js/designs.js
+++ js/designs.js
@@ -1,5 +1,8 @@
 (function($){
   $(function(){
     $(".button-collapse").sideNav();
+      
+    $('.invisible').fadeTo(0, 0).fadeTo(1500,1);
+      
   }); // end of document ready
 })(jQuery); // end of jQuery name space
